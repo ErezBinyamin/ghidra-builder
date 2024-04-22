41c3297c69fda08da78a291fc4da7ecd3a6d1417
Erez Binyamin <ezbin@pm.me>
2024-04-22 09:52:17 -0400

    build_ghidra.sh: Added PS4 and long args

--- workdir/build_ghidra.sh
+++ workdir/build_ghidra.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-set -ex
-
+export PS4='\033[0;33m$0:$LINENO [$?]+ \033[0m '
 START_DIR=$(pwd)
 OUTPUT_DIR=$START_DIR/out
+set -ex
 
-rm -rf $OUTPUT_DIR || true
-mkdir -p $OUTPUT_DIR
+if [[ -d ghidra || -f ghidra.bin || -d ${OUTPUT_DIR} ]]
+then
+	rm -rf ${OUTPUT_DIR} ghidra ghidra.bin || true
+fi
+mkdir --parents ${OUTPUT_DIR}
 
 if [ ! -d "ghidra" ]; then
     git clone https://github.com/NationalSecurityAgency/ghidra
 fi
 
-ln -s $HOME/ghidra.bin ghidra.bin # hack
+ln --symbolic ${HOME}/ghidra.bin ghidra.bin # hack
 
-cd $START_DIR/ghidra
+cd ${START_DIR}/ghidra
 gradle --init-script gradle/support/fetchDependencies.gradle init
 gradle yajswDevUnpack
 gradle buildGhidra
@@ -23,6 +26,6 @@ gradle buildGhidra
 # gradle unitTestReport # ~38 minutes
 # gradle integrationTest # ~3 hours
 
-cp build/dist/*.zip $OUTPUT_DIR
-unlink $START_DIR/ghidra.bin
+cp build/dist/*.zip ${OUTPUT_DIR}
+unlink ${START_DIR}/ghidra.bin
 
